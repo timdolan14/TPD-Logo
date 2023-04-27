85680a05fa82f49e4b900326f7c381be6ce17833
Tim Dolan <timmydolan14@gmail.com>
2023-04-27 17:38:53 -0400

    Updated File Structure and Updates to JS file

---
+++ index.js
@@ -0,0 +1,50 @@
+// Establish Variables
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { text } = require('stream/consumers');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up tp three characters here',
+
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Which color would you like to use for your text?',
+    },
+    {
+        type: 'input',
+        name: 'color',
+        message: 'Which color would you like for your shape?',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape would you like?',
+        choices: ['Square','Circle','Triangle'],
+    },
+];
+
+const writeToFile = ({text, color, shape}) =>
+`
+Shapes?
+
+`;
+
+
+// Function to Create File
+function init () {
+inquirer.prompt(questions)
+    .then((response) => {
+        console.log(response);
+        fs.writeFile('logo.svg', writeToFile(response), (err) => {
+            err ? console.log(err) : console.log("Succcess");
+        });
+    })
+}
+
+// Function call to initialize app
+init();
