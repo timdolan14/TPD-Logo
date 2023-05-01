29b9cb7813bcd0286331697066edd22d689cff06
Tim Dolan <timmydolan14@gmail.com>
2023-05-01 09:59:34 -0400

    Updated SVG Struc

--- Lib/fullLogo.js
+++ Lib/fullLogo.js
@@ -1,7 +1,11 @@
 const Shape = require("./shape")
 
 const fullLogo = (Shape) => { `
-<svg xmls="http:/www.w3.org/2000/svg" width ="400" height="400"> 
+<svg xmls="http:/www.w3.org/2000/svg" width ="400" height="400" version="1.1">
+
+<shape> 
+
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="_COLOR_">__TEXT__</text>
 
     ${Shape}
     ${text}
