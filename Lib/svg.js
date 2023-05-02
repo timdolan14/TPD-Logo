47f3e12d06c7d8fb9f0d25bc0ebc4cbc187ea980
Tim Dolan <timmydolan14@gmail.com>
2023-05-02 10:09:31 -0400

    Updated H x W

--- Lib/svg.js
+++ Lib/svg.js
@@ -5,7 +5,7 @@ class Svg {
         this.shape = shape;
     }
     render () {
-        return `<svg xmls="http:/www.w3.org/2000/svg" width ="400" height="400" version="1.1">
+        return `<svg xmls="http:/www.w3.org/2000/svg" height="300" width ="200" version="1.1">
         ${this.shape}
         <text x="150" y="165" font-size="40" text-anchor="middle" fill="${this.textColor}">${this.text}</text> </svg>
        `
